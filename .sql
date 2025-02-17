1ef8a1d62900581c2a95949a9d3541642188e8d2
ryanraidt <ryanraidt1026@gmail.com>
2025-02-16 22:17:06 -0600

    Deploying backend to Render

--- .sql
+++ .sql
@@ -2,4 +2,5 @@ SELECT current_database();
 
 SELECT * FROM "Recipe" WHERE "recipeId" IN (58, 59);
 
-SELECT * FROM "Activity" WHERE "userId" = (1 , 59, 58,);
+SELECT * FROM "Activity" WHERE "userId" IN (1, 59, 58);
+
