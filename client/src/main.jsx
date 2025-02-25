b3d72a213ab340da8049d55aceedf5a7a1941620
ryanraidt <ryanraidt1026@gmail.com>
2025-02-24 22:38:33 -0600

    Removed index.css import to fix build error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { SocketProvider } from "./SocketContext"; // Import SocketProvider
 import App from "./App";
-import "./index.css";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
